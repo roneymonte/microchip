9287a38925ebd03181167d4947af9070047997e8
roney <roneymonte@gmail.com>
2013-11-20 03:48:21 -0800

    projeto USART commit inicial

---
+++ PIC18/04_USART.X/main.c
@@ -0,0 +1,20 @@
+/*
+ * File:   main.c
+ * Author: roney
+ *
+ * Created on November 20, 2013, 9:40 AM
+ */
+
+
+#include <xc.h>
+#include <plib/usart.h>
+
+#include "configbits.h"
+
+void main(void) {
+
+
+
+
+    return;
+}
