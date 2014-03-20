30b639db5f4be4a852d93952b72c1f25412d2fac
roney <roneymonte@users.noreply.github.com>
2014-03-19 22:43:25 -0300

    Projeto 05_7SEG_Display.X

---
+++ PIC18/05_7SEG_Display.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=05_7SEG_Display.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/05_7SEG_Display.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=057segdisplay.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/057segdisplay.x.tar
