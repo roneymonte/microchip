1d7b18597dd682eb9da2d1e8d085b4bf0909d02c
roney <roneymonte@users.noreply.github.com>
2014-03-19 23:19:03 -0300

    Outros itens adicionais que estavam dando erro (build e dist) nao precisariam.

---
+++ PIC18/06_LCD_4550.X/build/default/production/main.p1.d
@@ -0,0 +1,8 @@
+ build/default/production/main.d  \
+ build/default/production/main.p1:  \
+ main.c  \
+WV_PIC_LIB/virtualwire.h  \
+config_bits.h  \
+interrupcoes.h  \
+def_funcoes.h  \
+WV_PIC_LIB/stdint.h 
