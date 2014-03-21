c96c0a7887df310a40df88c1e6fc3f907415cce7
roney <roneymonte@users.noreply.github.com>
2014-03-21 16:55:01 -0300

    Projeto I2C Master

---
+++ PIC18/08_PROTOCOLO_I2C_Master.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: roney
+ *
+ * Created on 21 de Março de 2014, 16:53
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ PIC18/08_PROTOCOLO_I2C_Master.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=08_PROTOCOLO_I2C_Master.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/08_PROTOCOLO_I2C_Master.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=08protocoloi2cmaster.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/08protocoloi2cmaster.x.tar
